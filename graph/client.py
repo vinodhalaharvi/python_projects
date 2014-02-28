e8ca74a30ffa628d78c63386fd918432e766581f
Vinod Halaharvi <vinod.halaharvi@gmail.com>
2014-02-28 11:10:07 -0500

    added BFS

--- graph/client.py
+++ graph/client.py
@@ -11,6 +11,9 @@
 # to copy or distribute this software to others!
 
 import sys
+from sets import Set
+from Queue import Queue
+
 
 def degree(G, v):
 	"""docstring for adj"""
@@ -28,16 +31,16 @@ def maxDegree(G):
 
 def averageDegree(G):
 	"""docstring for averageDegree"""
-	return 2.0 * G.E() /G.V(); 
+	return 2.0 * G.numE() /G.numV(); 
 
 def numberOfSelfLoops(G):
 	"""docstring for numberOfSelfLoops"""
 	count = 0 
-	for v in range(G.V()):
+	for v in range(G.numV()):
 		for w in G.adj(v):
 			if v == w:
 				count += 1
-	return count/2
+	return count
 
 
 class Graph(object):
@@ -54,18 +57,30 @@ class Graph(object):
 			for line in self.file.readlines():
 				_from,to  = line.strip().split(" ", 2)
 				self.addEdge(_from, to)
+        def E(self):
+                """docstring for E"""
+                for u in self.data.keys():
+                        for v in self.data[u]:
+                                yield u, v
+
+
+        def V(self):
+                """docstring for V"""
+                for u in self.data.keys():
+                        yield u
 
 	def __iter__(self, ):
 		for k, v in self.data.items():
 			for v1 in v:
 				yield k, v1
+                              
 
-	def V(self,):
-		"""docstring for V"""
+	def numV(self,):
+		"""docstring for numV"""
 		return len(self.data.values())
 
-	def E(self):
-		"""docstring for E"""
+	def numE(self):
+		"""docstring for numE"""
 		val = 0 
 		for key in self.data.keys():
 			val += len(self.data[key])
@@ -74,17 +89,18 @@ class Graph(object):
 	def adj(self, v):
 		return self.data[v]
 
-	def symbol(self, _from, to):
-		"""docstring for symbol"""
+	def xform(self, _from, to):
+		"""docstring for xform"""
 		return int(_from), int(to)
 
 	def addEdge(self, _from, to):
 		"""docstring for addEdge"""
-		_from, to = self.symbol(_from , to)
+		_from, to = self.xform(_from , to)
 		if _from in self.data:
-			self.data[_from].append(to)
+			#self.data[_from].append(to)
+			self.data[_from].add(to)
 		else:
-			self.data[_from] = [to,]
+			self.data[_from] = Set([to,])
 
 	def print_graph(self):
 		"""docstring for print_graph"""
@@ -93,56 +109,82 @@ class Graph(object):
 			print "%s->%s" % (_from, to)
 
 
-class Paths(object):
-	"""docstring for Paths"""
-	def __init__(self, arg):
-		super(Paths, self).__init__()
-		pass
-
-	def hasPathTo(self, v):
-		"""docstring for hasPathTo(int v"""
-		pass
-
-	def pathTo(self, v):
-		"""docstring for pathTo"""
-		pass
+class DFS(object):
+        """docstring for Dfs"""
+        def __init__(self, G, s):
+                super(Dfs, self).__init__()
+                self.G = G
+                self.s = s
 
 
-class DepthFirstPaths(object):
-	"""docstring for DepthFirstPaths"""
-	def __init__(self, G, s):
-		super(DepthFirstPaths, self).__init__()
-		self.marked = {}
-		for v in range(G.V()):
-			self.marked[v] = False
-		self.edgeTo = {}
-		self._dfs(G, s)
+class Vertex(object):
+        """docstring for Vertex"""
+        def __init__(self, u):
+                super(Vertex, self).__init__()
+                self.u = u
 
-	def _dfs(self, G, v):
-		"""docstring for dfs"""
-		self.marked[v] = True
-		for w in G.adj(v): 
-			if w in self.marked.keys() and not self.marked[w]:
-				self._dfs(G, w)
-				self.edgeTo[w] = v
-	def print_dfs_state(self):
-		"""docstring for print_dfs_state"""
-		print self.marked
-		print self.edgeTo
+        def __repr__(self):
+                """docstring for __repr__"""
+                return int(self.u)
+
+        def __str__(self):
+                """docstring for __str__"""
+                return self.u
+
+
+class BFS(object):
+        """docstring for BFS"""
+        def __init__(self):
+                super(BFS, self).__init__()
+
+        def bfs(G, s):
+                setV = Set(G.V())
+                for u in setV - Set(s):
+                        u.color = WHITE
+                        u.d = INF
+                        u.pi = None
+                s.color = GRAY
+                s.d = 0 
+                s.pi = None
+
+                Q = Queue()
+                Q.insert(s)
+                while Q:
+                        u = Q.remove()
+                        for v in G.adj(u):
+                                if v.color == WHITE:
+                                        v.color = GRAY
+                                        v.d = u.d + 1
+                                        v.pi = u 
+                                        Q.insert(v)
+                        u.color = BLACK
 
 
 if __name__ == '__main__':
 	g = Graph(sys.stdin)
 	g.print_graph()
+
+        print 
+        print "Edges are .." 
+        for edge in  g.E():
+                print edge
+        print
+
+        print "Vertices are .." 
+        for v in  g.V():
+                print v
+        print
+
+
+        '''
 	print 
 	for val in g:
 		print val
 	print
-		
 	print "maxDegree: %d" % maxDegree(g)
-	print "degree: %d" % degree(g, 1)
-	print "degree: %f" % averageDegree(g)
-	print "Number of self loops %d:" % numberOfSelfLoops(g)
+	print "degree of 1 is: %d" % degree(g, 1)
+	print "averageDegree: %f" % averageDegree(g)
+        print "Number of self loops: %d" % numberOfSelfLoops(g)
 	print 
-	DepthFirstPaths(g, 0).print_dfs_state()
+        '''
 	
